67331397b59867eecccd6f4dace57b16e17e0afa
Juan Pablo <jpag17@hotmail.com>
2021-08-09 12:19:59 -0500

    Actualizacion 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.efecto'); // .nav-list li
+	const menuAbierto = document.querySelectorAll('.efecto')[1];
+	const subMenu = document.querySelectorAll('.sub-menu ul')[0];
 
 	burger.addEventListener('click', () => {
 		// Toggle Nav
@@ -18,8 +20,51 @@ const navSlide = () => {
 
 		// Burger Animation
 		burger.classList.toggle('toggle');
+
+		// Se cierre el menu si se cierra la hamburgesa
+		navLinks.forEach((e) => {
+			if (e.classList.contains('activado')) {
+				e.classList.toggle('activado');
+			}
+		});
+	});
+
+	// Que se abra el menu con click
+	navLinks.forEach((e) => {
+		e.onclick = function() {
+			e.classList.toggle('activado');
+		};
 	});
 };
+
+function expandirMenu() {
+	const opcion = document.querySelector('.activado');
+
+	opcion.classList.toggle('.activado');
+}
+
+// Boton para ir arriba
+const scrollBtn = document.querySelector('.btn-arriba');
+
+const aparecerBoton = () => {
+	if (document.documentElement.scrollTop <= 200) {
+		scrollBtn.style.animation = `btnFadeAlReves 0.5s`;
+		scrollBtn.style.display = 'none';
+	} else {
+		scrollBtn.style.animation = `btnFade 0.5s`;
+		scrollBtn.style.display = 'block';
+	}
+};
+
+aparecerBoton();
+
+scrollBtn.addEventListener('click', () => {
+	document.documentElement.scrollTop = 0;
+});
+
+document.addEventListener('scroll', (e) => {
+	aparecerBoton();
+});
 
 const app = () => {
 	navSlide();
